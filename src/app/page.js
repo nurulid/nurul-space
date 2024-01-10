9742d470bfd6d3d5b0df385cfc7a49e65f31722e
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-01-10 20:23:30 +0700

    chore: setup nextui

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+import { Button } from "@nextui-org/react";
+
 export default function Home() {
   return (
-    <div>Hello world!</div>
+    <div>Hello world!
+      <Button>button</Button>
+    </div>
   )
 }
