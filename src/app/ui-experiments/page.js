44190361ff3a830d43b2110ceb3c4af48f469dcc
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-01-28 21:43:31 +0700

    feat: add portfolio source info

--- src/app/ui-experiments/page.js
+++ src/app/ui-experiments/page.js
@@ -1,11 +1,13 @@
+import { PortfolioInfo } from "@/components/portfolio/portfolioInfo";
 import { AllBadges } from "@/components/ui-experiments/allBadges";
 
 export default function Page() {
   return (
-    <div className='py-10 grid grid-cols-1 md:grid-cols-2'>
-      <div className="p-6 border border-gray-200 dark:border-gray-700 rounded-xl space-y-2 text-center">
+    <div className="py-10 grid grid-cols-1 md:grid-cols-2">
+      <div className="p-6 border border-gray-200 dark:border-gray-700 rounded-xl text-center">
         <AllBadges />
+        <PortfolioInfo />
       </div>
     </div>
-  )
+  );
 }
