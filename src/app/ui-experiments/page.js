9ff7b5fd20b48cbe2790d24cb4fee1e377407c74
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-02-07 23:01:41 +0700

    fix: grid layout and info component

--- src/app/ui-experiments/page.js
+++ src/app/ui-experiments/page.js
@@ -3,14 +3,39 @@ import { AllBadges } from "@/components/ui-experiments/allBadges";
 import { Search } from "@/components/ui-experiments/components/search";
 
 export default function Page() {
+  const uiExperiments = [
+    {
+      title: "Badges",
+      credit: {
+        name: "Orman Clark (@ormanclark)",
+        source: "https://x.com/ormanclark/status/1744379266731487296?s=20",
+      },
+      sourceCode: {
+        codepen: "https://codepen.io/nurulid/pen/zYbEMoz",
+        github: "https://github.com/nurulid/nurul-space/blob/main/src/components/ui-experiments/components/badge.jsx",
+      },
+    },
+    {
+      title: "Search",
+      credit: {
+        name: "Nur Asyrof Muhammad",
+        source: "https://dribbble.com/shots/17489923-Search/attachments/12627054?mode=media",
+      },
+      sourceCode: {
+        codepen: "",
+        github: "https://github.com/nurulid/nurul-space/blob/main/src/components/ui-experiments/components/search.jsx",
+      },
+    },
+  ];
   return (
-    <div className="py-10 grid grid-cols-1 md:grid-cols-2 gap-16">
-      <div className="p-6 border border-gray-200 dark:border-gray-700 rounded-xl">
+    <div className="py-10 columns-2 gap-14">
+      <div className="p-6 border border-gray-200 dark:border-gray-700 rounded-xl flex flex-col break-inside-avoid">
         <AllBadges />
-        <PortfolioInfo />
+        <PortfolioInfo data={uiExperiments[0]}/>
       </div>
-      <div className="p-6 border border-gray-200 dark:border-gray-700 rounded-xl bg-[#EFE9F5]">
+      <div className="p-6 border border-gray-200 dark:border-gray-700 rounded-xl flex flex-col break-inside-avoid">
         <Search />
+        <PortfolioInfo data={uiExperiments[1]}/>
       </div>
     </div>
   );
--- src/components/portfolio/portfolioInfo.jsx
+++ src/components/portfolio/portfolioInfo.jsx
@@ -1,34 +1,58 @@
 import { Codepen, Github } from "lucide-react";
 import Link from "next/link";
+import { websiteShortName } from "@/lib/utils";
 
-export const PortfolioInfo = () => {
+export const PortfolioInfo = ({ data }) => {
   return (
-    <div className="info pt-5 border-t dark:border-gray-700 mt-7 text-left text-sm text-gray-500 flex justify-between items-center">
-      <div className="space-y-2">
-        <h3>
-          Design by:{" "}
-          <span className="font-semibold text-black dark:text-gray-400">
-            Orman Clark (@ormanclark)
-          </span>
-        </h3>
-        <p>
-          Source:{" "}
-          <a
-            href="https://x.com/ormanclark/status/1744379266731487296?s=20"
-            target="_blank"
-            className="text-violet-600 hover:text-yellow-600"
-          >
-            x.com
-          </a>
-        </p>
+    <div className="mt-auto">
+      <div className="mt-8 text-left">
+        <h2 className="text-xl">{data.title}</h2>
       </div>
-      <div className="space-y-2">
-        <a href="https://codepen.io/nurulid/pen/zYbEMoz" target="_blank" className="block" title="Codepen">
-          <Codepen size={25} className="p-1 bg-violet-100 hover:bg-yellow-100 dark:bg-gray-800 dark:hover:bg-gray-800/50 dark:border-gray-700 rounded-md"/>
-        </a>
-        <a href="https://github.com/nurulid/nurul-space/blob/main/src/components/ui-experiments/components/badge.jsx" target="_blank" className="block" title="GitHub">
-          <Github size={25} className="p-1 bg-violet-100 hover:bg-yellow-100 dark:bg-gray-800 dark:hover:bg-gray-800/50 dark:border-gray-700 rounded-md"/>
-        </a>
+      <div className="info pt-4 border-t dark:border-gray-700 mt-4 text-left text-sm text-gray-500 flex justify-between items-center">
+        <div className="space-y-2">
+          <h3>
+            Design by:{" "}
+            <span className="font-semibold text-black dark:text-gray-400">
+              {data.credit.name}
+            </span>
+          </h3>
+          <p>
+            Source:{" "}
+            <a
+              href={data.credit.source}
+              target="_blank"
+              className="text-violet-600 hover:text-yellow-600"
+            >
+              {websiteShortName(data.credit.source)}
+            </a>
+          </p>
+        </div>
+        <div className="space-y-2">
+          {data.sourceCode.codepen && (
+            <a
+              href={data.sourceCode.codepen}
+              target="_blank"
+              className="block"
+              title="Codepen"
+            >
+              <Codepen
+                size={25}
+                className="p-1 bg-violet-100 hover:bg-yellow-100 dark:bg-gray-800 dark:hover:bg-gray-800/50 dark:border-gray-700 rounded-md"
+              />
+            </a>
+          )}
+          <a
+            href={data.sourceCode.github}
+            target="_blank"
+            className="block"
+            title="GitHub"
+          >
+            <Github
+              size={25}
+              className="p-1 bg-violet-100 hover:bg-yellow-100 dark:bg-gray-800 dark:hover:bg-gray-800/50 dark:border-gray-700 rounded-md"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
