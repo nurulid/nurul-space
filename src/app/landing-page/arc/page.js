7ae4c308a35c5d80da9565ca2d42dea35dd009f8
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-02-29 11:35:44 +0700

    lp: arc footer

--- src/app/landing-page/arc/page.js
+++ src/app/landing-page/arc/page.js
@@ -1,13 +1,15 @@
-import { Header } from '@/components/arc/header'
-import { HeroHome } from '@/components/arc/heroHome'
-import React from 'react'
+import { Footer } from "@/components/arc/footer";
+import { Header } from "@/components/arc/header";
+import { HeroHome } from "@/components/arc/heroHome";
+import React from "react";
 
 export default function Page() {
   return (
     <>
-      <Header/>
-      <HeroHome/>
-      <main className='max-w-7xl w-full mx-auto h-full py-6 px-5 lg:px-10'></main>
+      <Header />
+      <HeroHome />
+      {/* <main className="max-w-7xl w-full mx-auto h-full py-6 px-5 lg:px-10"></main> */}
+      <Footer />
     </>
-  )
+  );
 }
