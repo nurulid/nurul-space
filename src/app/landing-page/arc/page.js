1b4bc7a4ad4b7ce4bfa33a3c9c4cad93c5d61a0a
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-02-29 09:50:28 +0700

    lp: arc home hero

--- src/app/landing-page/arc/page.js
+++ src/app/landing-page/arc/page.js
@@ -1,10 +1,12 @@
 import { Header } from '@/components/arc/header'
+import { HeroHome } from '@/components/arc/heroHome'
 import React from 'react'
 
 export default function Page() {
   return (
     <>
       <Header/>
+      <HeroHome/>
       <main className='max-w-7xl w-full mx-auto h-full py-6 px-5 lg:px-10'></main>
     </>
   )
