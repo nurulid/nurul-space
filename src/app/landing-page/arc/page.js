76c0f489d70ce809e290f175e2e2f4df166febc1
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-02-29 09:45:02 +0700

    lp: arc header

---
+++ src/app/landing-page/arc/page.js
@@ -0,0 +1,11 @@
+import { Header } from '@/components/arc/header'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <>
+      <Header/>
+      <main className='max-w-7xl w-full mx-auto h-full py-6 px-5 lg:px-10'></main>
+    </>
+  )
+}
