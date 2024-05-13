840ef10c40d3ed534560c13d440505a3f64ab3e3
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-05-13 20:45:33 +0700

    add header code block

---
+++ src/components/mdx/codeHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CodeHeader = ({text}) => {
+  return (
+    <div className="rounded-t-md bg-zinc-700 px-4 py-2 font-mono text-sm text-neutral-300 dark:bg-zinc-700 dark:text-neutral-300">{text}</div>
+  )
+}
