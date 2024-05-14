e815321ff31020ab8dda374092d4090ecdf696f4
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-05-14 16:29:15 +0700

    add resume source code

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -6,6 +6,7 @@ import { Work } from '@/components/resume/work';
 import { Skills } from '@/components/resume/skills';
 import { Portfolio } from '@/components/resume/portfolio';
 import { Education } from '@/components/resume/education';
+import { Github } from 'lucide-react';
 
 const resume = RESUME_DATA;
 
@@ -24,13 +25,27 @@ export default function Page() {
         <Education data={resume.education} />
         <Skills data={resume.skills} />
         <Portfolio data={resume.portfolio} />
-        <div className='max-w-[500px] w-full mx-auto border-b-[0.5px] border-gray-200 decoration-wavy pt-10'></div>
       </div>
-      <div className="mt-14 px-5 text-gray-600 text-xs text-center">
-        Design of this resume credit to{' '}
-        <a href="https://cv.jarocki.me/" target='_blank' className="underline hover:opacity-80">
-          Bartosz Jarocki
-        </a>{' '}🙌
+      <div className="max-w-[500px] w-full mx-auto border-t-[0.5px] border-gray-200 mt-10 pt-5 text-gray-600 flex justify-between items-center">
+          <a
+            href="https://github.com/nurulid/nurul-space/tree/main/src/components/resume"
+            title="Source code" 
+            target='_blank'
+            className='hover:text-violet-600 transition-all'
+          >
+            <Github size={18}/>
+          </a>
+          <p className='text-xs'>
+            Design of this resume credit to{' '}
+            <a
+              href="https://cv.jarocki.me/"
+              target="_blank"
+              className="underline hover:opacity-80"
+            >
+              Bartosz Jarocki
+            </a>{' '}
+            🙌
+          </p>
       </div>
     </>
   );
