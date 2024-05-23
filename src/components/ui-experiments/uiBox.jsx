1320e90dded772a076949094b368cba330bfb898
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-05-23 21:12:34 +0700

    up

--- src/components/ui-experiments/uiBox.jsx
+++ src/components/ui-experiments/uiBox.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { UiDesignInfo } from './components/uiDesignInfo';
 
-export const UiBox = ({ui, uiInfo}) => {
+export const UiBox = ({ui, uiInfo, title}) => {
   return (
-    <div className="p-6 border border-gray-200 dark:border-gray-700 rounded-xl flex flex-col">
+    <div id={title} className="p-6 border border-gray-200 dark:border-gray-700 rounded-xl flex flex-col">
       {ui}
       <UiDesignInfo data={uiInfo} />
     </div>
--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 export async function middleware(req) {
   const { pathname } = req.nextUrl;
   if (pathname == '/ui-experiments') {
-    return NextResponse.redirect(new URL('/ui', req.url));
+    return NextResponse.redirect(new URL('/ui/#badge', req.url));
   }
   return NextResponse.next();
 }
