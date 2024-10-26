19f42bf7ba3ed915eb323fe3740ace45450b5519
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-10-26 22:33:15 +0700

    updated writing content list

--- src/app/writing/page.js
+++ src/app/writing/page.js
@@ -2,6 +2,17 @@ import { Dot } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
+const writingPosts = [
+  {
+    id: 1,
+    title: 'How to add an active state in Tailwind CSS',
+    url: '/writing/tailwind-active-state',
+    created: '25 october, 2024',
+    tag: 'tailwind',
+    description: 'Some ways how to add an active state in Tailwind',
+  },
+];
+
 export default function Page() {
   const dummy = Array.from({ length: 5 });
 
@@ -10,26 +21,30 @@ export default function Page() {
       <div>
         <h2 className="text-2xl font-semibold flex items-center">
           <span>Writings</span> <Dot />
-          <span className="text-xs opacity-50">{dummy.length} posts</span>
+          <span className="text-xs opacity-50">
+            {writingPosts.length} posts
+          </span>
         </h2>
-        <p className='opacity-75'>Sharing some tips that related to frontend web.</p>
+        <p className="opacity-75">
+          Sharing some tips that related to frontend web.
+        </p>
       </div>
       <div className="space-y-8">
-        {dummy.map((_, i) => {
+        {writingPosts.map((post, i) => {
           return (
             <div key={i} className="space-y-1">
-              <span className="uppercase text-xs opacity-50">tailwind</span>
+              <span className="uppercase text-xs opacity-50">{post.tag}</span>
               <div className="space-y-1">
-                <Link href="/writing/tailwind-active-state" className="text-lg font-medium">Title lorem ipsum</Link>
+                <Link href={post.url} className="text-lg font-medium">
+                  {post.title}
+                </Link>
                 <p className="opacity-75 text-sm line-clamp-2">
-                  Description lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit, sed do eiusmod tempor incididunt ut labore et dolore
-                  magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat
+                  {post.description}
                 </p>
               </div>
-              <span className="opacity-50 text-xs">24 June, 2024</span>
+              <span className="opacity-50 text-xs capitalize">
+                {post.created}
+              </span>
             </div>
           );
         })}
