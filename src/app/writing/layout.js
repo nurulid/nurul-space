ea1db4dbf47299589a1958d0b81fb19e2c0e21f0
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-11-04 20:42:48 +0700

    tweaks

--- src/app/writing/layout.js
+++ src/app/writing/layout.js
@@ -18,7 +18,7 @@ export default function MdxLayout({ children }) {
         <>
           <Link
             href="/writing"
-            className="group p-1 size-[34px] absolute -left-16 top-9 flex items-center justify-center bg-purple-50 rounded-md"
+            className="group p-1 size-[34px] static sm:absolute -left-16 top-9 flex items-center justify-center bg-purple-50 rounded-md"
           >
             <ArrowLeft
               size={20}
@@ -29,7 +29,7 @@ export default function MdxLayout({ children }) {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ type: "tween" }}
-            className="-mt-12"
+            className="-mt-8 sm:-mt-12"
           >
             {children}
           </motion.article>
