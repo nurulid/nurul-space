e595961bd9ea407116e234cbd13016e1a7fdf875
Nurul Insani Dewi <nurulinsanid@gmail.com>
2025-03-03 20:24:36 +0700

    update content

--- src/app/ui/page.js
+++ src/app/ui/page.js
@@ -1,16 +1,37 @@
-import { UI_EXPERIMENTS } from "@/data/ui-experiments";
-import { AllBadges } from "@/components/ui-experiments/allBadges";
-import { Search } from "@/components/ui-experiments/search";
-import { ShareDialog } from "@/components/ui-experiments/shareDialog";
-import { UiBox } from "@/components/ui-experiments/uiBox";
+import { UI_EXPERIMENTS } from '@/data/ui-experiments';
+import { AllBadges } from '@/components/ui-experiments/allBadges';
+import { Search } from '@/components/ui-experiments/search';
+import { ShareDialog } from '@/components/ui-experiments/shareDialog';
+import { UiBox } from '@/components/ui-experiments/uiBox';
+import Link from 'next/link';
 
 export default function Page() {
   const uiExperiments = UI_EXPERIMENTS;
   return (
-    <div className="py-0 md:py-10 space-y-6 lg:space-y-10">
-      <UiBox ui={<ShareDialog />} uiInfo={uiExperiments[2]} title="share-dialog"/>
-      <UiBox ui={<Search />} uiInfo={uiExperiments[1]} title="search"/>
-      <UiBox ui={<AllBadges />} uiInfo={uiExperiments[0]} title="badge"/>
-    </div>
+    <>
+      <div className="space-y-2">
+        <h1 className="text-3xl">UI</h1>
+        <p>
+          <span className="opacity-50">Check more UI at my </span>
+          <Link
+            href="https://nid-ui-component-collections.vercel.app/"
+            target="_blank"
+            className="text-violet-500 hover:underline transition-all"
+          >
+            UI Collections.
+          </Link>
+        </p>
+      </div>
+      <div className="h-8" />
+      <div className="pb-4 md:pb-10 space-y-6 lg:space-y-10">
+        <UiBox
+          ui={<ShareDialog />}
+          uiInfo={uiExperiments[2]}
+          title="share-dialog"
+        />
+        <UiBox ui={<Search />} uiInfo={uiExperiments[1]} title="search" />
+        <UiBox ui={<AllBadges />} uiInfo={uiExperiments[0]} title="badge" />
+      </div>
+    </>
   );
 }
--- src/components/portfolio/Intro.jsx
+++ src/components/portfolio/Intro.jsx
@@ -1,16 +1,18 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import {
-  Github,
-  Linkedin,
-  Mail,
-  Dribbble,
-} from 'lucide-react';
+import { Github, Linkedin, Mail, Dribbble } from 'lucide-react';
+import { XIcon } from '@/components/resume/icons/xIcon';
 
 import { LinkIcon } from '@/components/ui/linkIcon';
 
 const contact = [
+  {
+    title: 'X',
+    url: 'http://x.com/nurulide',
+    icon: XIcon,
+    email: false,
+  },
   {
     title: 'Dribbble',
     url: 'https://dribbble.com/nurulid',
@@ -34,54 +36,45 @@ const contact = [
 
 export const Intro = () => {
   return (
-    <div
-      className="mt-10 mb-6 max-w-3xl ml-auto pb-10 text-center md:text-right leading-6"
-    >
-      <motion.h2 
-        initial={{ opacity: 0, y: -100}}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{type: 'spring', delay: 1}}
-        className="mb-2 text-4xl lg:text-5xl text-center md:text-right font-mono">
+    <div className="mt-10 mb-6 max-w-3xl ml-auto pb-10 text-center md:text-right leading-6">
+      <h2 className="mb-2 text-4xl lg:text-5xl text-center md:text-right font-mono">
         Hello 🙂 <br />
-        I'm <span className="text-purple-500">Nurul</span> a Frontend Developer from Indonesia.
-      </motion.h2>
-      <motion.p 
-        initial={{ opacity: 0, y: -10}}
+        I'm <span className="text-purple-500">Nurul</span> a Frontend Developer
+        from Indonesia.
+      </h2>
+      <p
+        initial={{ opacity: 0, y: -10 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{type: 'spring', delay: 1.5, stiffness: 500}}
-        className="text-gray-500 dark:text-gray-400 text-center md:text-right leading-7">
-        I’ve worked as a Frontend Developer for 4+ years, especially in UI (User Interface). During my experience, I learn and practice how to build beautiful and functional products.
-      </motion.p>
+        transition={{ type: 'spring', delay: 1.5, stiffness: 500 }}
+        className="text-gray-500 dark:text-gray-400 text-center md:text-right leading-7"
+      >
+        I’ve worked as a Frontend Developer for 4+ years, especially in UI (User
+        Interface). During my experience, I learn and practice how to build
+        beautiful and functional products.
+      </p>
 
-        <motion.div 
-        initial={{ opacity: 0, y: -10}}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{type: 'spring', delay: 2, stiffness: 500}} className="relative inline group">
-          <a
-            href="mailto:call.nurulid@gmail.com"
-            title="Get in touch"
-            className="text-right inline-block mt-2 mb-4 py-3 px-2 bg-violet-50 dark:bg-violet-800/40 border border-violet-100 dark:border-gray-800 rounded-xl w-[80%] sm:w-[30%]"
-          >
-            <span className="rounded-md px-6 py-2 bg-white dark:bg-white/90 dark:hover:bg-white shadow-md group-hover:shadow-sm dark:text-black transition-all block text-center">
-              Get In Touch
-            </span>
-          </a>
-          <div className="absolute left-2 -top-3">
-            <div className="relative flex h-3 w-3">
-              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-              <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-            </div>
+      <div className="relative inline group">
+        <a
+          href="mailto:call.nurulid@gmail.com"
+          title="Get in touch"
+          className="text-right inline-block mt-2 mb-4 py-3 px-2 bg-violet-50 dark:bg-violet-800/40 border border-violet-100 dark:border-gray-800 rounded-xl w-[80%] sm:w-[30%]"
+        >
+          <span className="rounded-md px-6 py-2 bg-white dark:bg-white/90 dark:hover:bg-white shadow-md group-hover:shadow-sm dark:text-black transition-all block text-center">
+            Get In Touch
+          </span>
+        </a>
+        <div className="absolute left-2 -top-3">
+          <div className="relative flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
           </div>
-        </motion.div>
-        <motion.div 
-        initial={{ opacity: 0, y: -10}}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{type: 'spring', delay: 2.5, stiffness: 500}}
-        className="flex justify-center md:justify-end gap-4 items-center mt-4">
-          {contact.map(({ title, url, icon: Icon, email }, i) => (
-            <LinkIcon key={i} {...{ title, url, Icon, email }} />
-          ))}
-        </motion.div>
+        </div>
+      </div>
+      <div className="flex justify-center md:justify-end gap-4 items-center mt-4">
+        {contact.map(({ title, url, icon: Icon, email }, i) => (
+          <LinkIcon key={i} {...{ title, url, Icon, email }} />
+        ))}
+      </div>
     </div>
   );
 };
